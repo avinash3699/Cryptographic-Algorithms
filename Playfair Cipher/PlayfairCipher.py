e6673cb5f2fbe16f03fdf53c220ae37c91e6ab69
Riduvarshini S R <61472369+Riduvarshini-S-R@users.noreply.github.com>
2022-12-03 20:26:44 +0530

    Update PlayfairCipher.py

--- Playfair Cipher/PlayfairCipher.py
+++ Playfair Cipher/PlayfairCipher.py
@@ -40,9 +40,9 @@ def swaps(matrix,pt):
                 elif pt[i+1] in matrix[j][k]:
                     r2,c2=j,k
         if r1==r2:
-            ciphertext+=(matrix[r1][c1+1]+matrix[r2][c2+1])
+            ciphertext+=(matrix[r1][(c1+1)%5]+matrix[r2][(c2+1)%5])
         elif c1==c2:
-            ciphertext+=(matrix[r1+1][c1]+matrix[r2+1][c2])
+            ciphertext+=(matrix[(r1+1)%5][c1]+matrix[(r2+1)%5][c2])
         else:
             ciphertext+=(matrix[r1][c2]+matrix[r2][c1])
     return ciphertext
