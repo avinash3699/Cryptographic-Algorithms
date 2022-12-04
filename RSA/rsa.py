43484d67367fc453493eca13f7c061667daf8b9a
Riduvarshini S R <61472369+Riduvarshini-S-R@users.noreply.github.com>
2022-12-04 15:06:56 +0530

    rsa.py

---
+++ RSA/rsa.py
@@ -0,0 +1,15 @@
+p,q=map(int,input("Enter 2 prime numbers: ").split())
+n=p*q
+phifunc=(p-1)*(q-1)
+message=int(input("Enter the message: "))
+e=int(input("Enter a number e which is relatively prime ot phifunc: "))
+d=1 
+s=(d*e)%phifunc
+while s!=1:
+    d+=1 
+    s=(d*e)%phifunc 
+
+encryptedtext=(message**e)%n 
+print("Ciphertext is:",encryptedtext)
+decryptedtext=(encryptedtext**d)%n
+print("Plaintext is:",decryptedtext)
